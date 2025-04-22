44b2d55ea0daa862fed0b5a96efe852c07a4fcd0
Florian Wunsch <Florian95.wunsch@gmail.com>
2025-04-22 08:40:33 +0200

    Done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length){
+      throw new Error("OutoFBounds");
+    } return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if( this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.length -1];
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList")
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if (this.length === 0){
+      return 0;
+    }
+    return this.items.reduce((add, item) => add + item, 0)
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      return 0;
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
